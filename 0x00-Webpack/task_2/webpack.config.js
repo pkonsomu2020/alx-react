9a1fa9b8fea5ba0dce9f6828e036b5a0e9a80933
pkonsomu2020 <pkonsomu2020@gmail.com>
2023-08-22 22:44:43 -0700

    Task 2

---
+++ 0x00-Webpack/task_2/webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+module.exports = {
+  entry: './js/dashboard_main.js',
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js',
+  },
+  mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        loader: 'file-loader',
+        options: {
+          name: '[name].[ext]',
+          outputPath: 'images',
+        },
+      },
+    ],
+  },
+};
