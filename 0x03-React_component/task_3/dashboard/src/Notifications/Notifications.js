4315f320c0a214dc293cdd5adc99a4378b382813
pkonsomu2020 <113601561+pkonsomu2020@users.noreply.github.com>
2023-09-04 19:11:39 +0300

    Update Notifications.js

--- 0x03-React_component/task_3/dashboard/src/Notifications/Notifications.js
+++ 0x03-React_component/task_3/dashboard/src/Notifications/Notifications.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import './Notifications.css';
 import closeIcon from '../assets/close-icon.png';
 import NotificationItem from './NotificationItem';
 import PropeTypes from 'prop-types';
 import NotificationItemShape from './NotificationItemShape';
+import { StyleSheet, css } from 'aphrodite/no-important';
 
 
 class Notifications extends React.Component {
@@ -16,26 +16,19 @@ class Notifications extends React.Component {
     console.log(`Notification ${id} has been marked as read`);
   }
 
+  shouldComponentUpdate(nextProps) {
+    return nextProps.listNotifications.length > this.props.listNotifications.length;
+  }
+
   render() {
     return (
-      <div className='wrapper'>
-        <div className='menuItem'>
+      <div className={css(styles.Wrapper)}>
+        <div className={css(styles.menuItem)}>
           Your notifications
         </div>
         {this.props.displayDrawer? 
-            <div className="Notifications">
-              <button style={{
-                color: '#3a3a3a',
-                fontWeight: 'bold',
-                background: 'none',
-                border: 'none',
-                fontSize: '15px',
-                position: 'absolute',
-                right: '3px',
-                top: '3px',
-                cursor: 'pointer',
-                outline: 'none',
-              }}
+            <div className={css(styles.Notifications)}>
+              <button className={css(styles.NotificationsBtn)}
               aria-label="Close"
               onClick={(e) => {
                 console.log('Close button has been clicked');
@@ -45,7 +38,7 @@ class Notifications extends React.Component {
               </button>
               {
                 this.props.listNotifications.length != 0 ?
-                  <p>Here is the list of notifications</p>
+                  <p className={css(styles.notiHeader)}>Here is the list of notifications</p>
                 : null
               }
               <ul>
@@ -75,6 +68,62 @@ class Notifications extends React.Component {
     );
   }
 }
+
+const smallScreen = {
+  small: '@media screen and (max-width: 900px)'
+};
+
+const styles = StyleSheet.create({
+  menuItem: {
+		fontFamily: 'Arial, Helvetica, sans-serif',
+		textAlign: 'right',
+		fontWeight: 'bold',
+		padding: '5px',
+    [smallScreen.small]: {
+      display: 'none'
+    }
+	},
+  notiHeader: {
+    width: '100%'
+  },
+  Notifications: {
+    fontFamily: 'Arial, Helvetica, sans-serif',
+    border: '3px dotted #e1484c',
+    padding: '6px 12px',
+    position: 'relative',
+    marginTop: 12,
+    [smallScreen.small]: {
+      display: 'block',
+      width: '100vw',
+      height: '100vh',
+      fontSize: 20,
+      background: '#fff',
+      padding: 0,
+      border: 'none',
+      margin: 0,
+      listStyle: 'none'
+    }
+  },
+  NotificationsBtn: {
+    color: '#3a3a3a',
+    fontWeight: 'bold',
+    background: 'none',
+    border: 'none',
+    fontSize: '15px',
+    position: 'absolute',
+    right: '3px',
+    top: '3px',
+    cursor: 'pointer',
+    outline: 'none',
+  },
+  Wrapper: {
+    width: '100%',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'flex-end',
+    justifyContent: 'end'
+  }
+})
 
 Notifications.defaultProps = {
   displayDrawer: false,
