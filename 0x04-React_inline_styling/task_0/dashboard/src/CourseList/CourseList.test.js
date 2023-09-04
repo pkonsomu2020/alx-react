abaa298be65c0ef872bf1eafbcb84947f3ffe2a4
pkonsomu2020 <113601561+pkonsomu2020@users.noreply.github.com>
2023-09-04 18:00:40 +0300

    Update CourseList.test.js

--- 0x04-React_inline_styling/task_0/dashboard/src/CourseList/CourseList.test.js
+++ 0x04-React_inline_styling/task_0/dashboard/src/CourseList/CourseList.test.js
@@ -1,16 +1,18 @@
 import React from 'react';
-import CourseList from './CourseList';
 import { shallow } from 'enzyme';
+import { expect } from 'chai';
+import CourseListRow from './CourseListRow';
 
-describe('CourseList component', () => {
-  it('renders without crashing', () => {
-    const wrapper = shallow(<CourseList/>);
-    expect(wrapper.exists()).toBe(true);
-  })
+describe('CourseList render', () => {
+    it('CourseList should render without errors', () => {
+        const wrapper = shallow(<CourseListRow />);
+        expect(wrapper.exists()).equal(true);
+    })
 
-  it('renders the 5 different rows', () => {
-    const wrapper = shallow(<CourseList />)
-    expect(wrapper.find('table thead').children().length).toEqual(2);
-    expect(wrapper.find('table tbody').children().length).toEqual(1);
-  })
+    it('CourseList should render one cell with colspan=2 when textSecondCell does not exist', () => {
+        const wrapper = shallow(<CourseListRow isHeader={true} textSecondCell={null} testFirstCell='cell' />);
+
+        expect(wrapper.find('tr').children()).to.have.lengthOf(1);
+        expect(wrapper.find('th').props()).to.have.property('colSpan', '2');
+    })
 })
