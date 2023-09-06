b4e79962efd33bff022097af21b79cc8e6955c09
pkonsomu2020 <113601561+pkonsomu2020@users.noreply.github.com>
2023-09-06 09:41:54 +0300

    Update Header.js

--- 0x05-react_state/task_2/dashboard/src/Header/Header.js
+++ 0x05-react_state/task_2/dashboard/src/Header/Header.js
@@ -1,12 +1,56 @@
-import React from 'react';
 import holbertonLogo from '../assets/holberton_logo.jpg';
-import './header.css';
+import React, {useContext} from 'react';
+import { StyleSheet, css } from 'aphrodite';
+import { AppContext } from '../App/AppContext';
 
-export default function Header() {
+
+const styles = StyleSheet.create({
+  header: {
+    display: "flex",
+    alignItems: "center",
+    marginBottom: 60
+  },
+  
+  img: {
+    width: 160,
+    height: 160
+  },
+  
+  heading: {
+    color: "#E0354B"
+  },
+
+  logOut: {
+    fontStyle: "italic",
+    textDecoration: "underline",
+    cursor: "pointer"
+  }
+
+})
+
+export default class Header extends React.Component {
+  // static contextType = AppContext
+  render() {
+    const data = this.context
+    const email = data.currentUser.email
+    const displayText = () => {
+      if (data.currentUser.isLoggedIn){
+        return (
+        <section id="logoutSection">Welcome {email}
+          <span  className={css(styles.logOut)} onClick={data.logOut}>(logout)</span>
+        </section>
+        )
+      }
+    }
     return (
-        <header className="App-header">
-            <img src={holbertonLogo} alt="holberton-logo"></img>
-            <h1>School dashboard</h1>
-        </header>
-    );
+      <React.Fragment>
+      <div className={css(styles.header)}>
+        <img className={css(styles.img)} src={holbertonLogo} alt="logo"/>
+        <h1 className={css(styles.heading)}>School dashboard</h1>
+      </div>
+      {displayText()}
+      </React.Fragment>
+    )
+  }
 }
+Header.contextType = AppContext
