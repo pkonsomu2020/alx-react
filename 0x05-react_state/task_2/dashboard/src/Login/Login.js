de2c26692892ff5cda12d65e300485378c0efc85
pkonsomu2020 <113601561+pkonsomu2020@users.noreply.github.com>
2023-09-06 09:41:03 +0300

    Update Login.js

--- 0x05-react_state/task_2/dashboard/src/Login/Login.js
+++ 0x05-react_state/task_2/dashboard/src/Login/Login.js
@@ -1,26 +1,58 @@
 import React from 'react';
+import { StyleSheet, css } from 'aphrodite';
 
-export default function Login() {
+const styles = StyleSheet.create({
+  login:{
+    '@media (max-width: 900px)': {
+      display: "block"
+    }
+  }
+})
+
+export default class Login extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      email: '',
+      password: '',
+      enableSubmit: false
+    }
+  }
+
+  handleLoginSubmit = (event) => {
+      console.log('submitted')
+      this.props.logIn(this.state.email, this.state.password)
+      event.preventDefault()
+    
+  }
+
+  handleChangeEmail=(event)=> {
+    this.setState({email: event.target.value}, this.submitButtonState)
+  }
+
+  handleChangePassword = (event) => {
+    this.setState({password: event.target.value}, this.submitButtonState)
+  }
+
+  submitButtonState() {
+    if (this.state.email.length > 0 && this.state.password.length > 0) {
+      this.setState({enableSubmit: true})
+    } else {
+      this.setState({enableSubmit: false})
+    }
+  }
+
+  render() {
     return (
-        <div className="App-body">
-            <p>
-                Login to access the full dashboard
-            </p>
-            <form style={{display: 'flex', gap: '1rem'}}>
-                <div>
-                    <label htmlFor="email" style={{marginRight: '5px'}}>
-                        Email
-                    </label>
-                    <input type="email" name="email" style={{border: '1px solid #D3D3D3', borderRadius: '3px'}}/>
-                </div>
-                <div>
-                    <label htmlFor="password" style={{marginRight: '5px'}}>
-                        Password
-                    </label>
-                    <input type="password" name="password" style={{border: '1px solid #D3D3D3', borderRadius: '3px'}}/>
-                </div>
-                <button type="submit" style={{border: '1px solid #D3D3D3', borderRadius: '3px', background: 'transparent'}}>OK</button>
-            </form>
-        </div>
+      <form onSubmit={this.handleLoginSubmit}>
+        <label className={css(styles.login)} htmlFor="email">
+          Email: <input type="text" id="email" name="email" value={this.state.email} onChange={this.handleChangeEmail}/>
+        </label>
+        <label className={css(styles.login)} htmlFor="password"> 
+          Password: <input type="text" id="password" value={this.state.password} onChange={this.handleChangePassword}/>
+        </label>
+        <input type="submit" value="OK" disabled={this.state.enableSubmit ? false : true}/>
+      </form>
     )
+  }
 }
