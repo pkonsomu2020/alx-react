e4fb6190531d11ebf99bd7c60d4ec9c29b6b08a7
pkonsomu2020 <113601561+pkonsomu2020@users.noreply.github.com>
2023-09-06 09:51:26 +0300

    Update Footer.js

--- 0x05-react_state/task_3/dashboard/src/Footer/Footer.js
+++ 0x05-react_state/task_3/dashboard/src/Footer/Footer.js
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { getFullYear, getFooterCopy } from '../utils/utils';
-import './Footer.css';
+import { StyleSheet, css } from 'aphrodite/no-important';
+import { AppContext } from '../App/AppContext';
+
+const styles = StyleSheet.create({
+    bottom: {
+        width: '100%'
+    },
+    footerParagraph: {
+        color: 'white',
+        textAlign: 'center',
+        borderTop: '2px solid #e1484c'
+    }
+})
 
 function Footer() {
+    const context = React.useContext(AppContext);
+    const footerText = (context.currentUser.isLoggedIn) ? <a href="#">Contact us</a> : `Copyright ${getFullYear()} - ${getFooterCopy(false)}`
     return (
-        <div className="App-footer">
-            <p>Copyright {getFullYear()} - {getFooterCopy()}</p>
+        <div className={css(styles.bottom)}>
+            <p className={css(styles.footerParagraph)}>{footerText}</p>
         </div>
     )
 }
